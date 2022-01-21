d05cfc82991540a3afcf907853811c1b866389cf
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-01-21 19:57:35 -0300

    Taller, uso de interface, herencia, polimorfismo

---
+++ Java/ProyectoTaller/src/Taller.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Taller {
+	ArrayList<Vehiculo> vehiculos;
+	
+	public Taller()
+	{
+		this.vehiculos = new ArrayList<Vehiculo>();
+	}
+	
+	public boolean agregar(Vehiculo vehiculo)
+	{
+		return false;		
+	}
+	public String listar()
+	{
+		return "";
+	}
+	public String contabilizar()
+	{//???? como recorrer una colección????  --> for 
+		return "5 motos, 2 autos";
+	}
+	
+}
