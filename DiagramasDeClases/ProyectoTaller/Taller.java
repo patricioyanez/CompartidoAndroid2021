376053b25a6257593d76fc017d9f47a75d52b247
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-01-21 19:39:10 -0300

    diagrama

---
+++ DiagramasDeClases/ProyectoTaller/Taller.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Taller here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Taller
+{
+    private ArrayList<Vehiculo> vehiculos;
+
+    /**
+     * Constructor for objects of class Taller
+     */
+    public Taller()
+    {
+        vehiculos = new ArrayList<Vehiculo>();
+    }
+    
+  
+}
---
+++ DiagramasDeClases/ProyectoTaller/Vehiculo.java
@@ -0,0 +1,19 @@
+/**
+ * Abstract class Automovil - write a description of the class here
+ * 
+ * @author: 
+ * Date: 
+ */
+public abstract class Vehiculo implements IDescuentos  
+{
+    // instance variables - replace the example below with your own
+    double IVA = 0.19;
+    double DESCUENTO_AUTOMOVIL= 0.5;
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the sum of x and y 
+     */
+    public abstract int sampleMethod(int y);
+}
