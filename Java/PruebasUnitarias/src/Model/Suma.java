a2ecc4ee9e22097e7ae891bfd6b37f7c2ca87a0c
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-01-26 20:16:08 -0300

    Update Suma.java

--- Java/PruebasUnitarias/src/Model/Suma.java
+++ Java/PruebasUnitarias/src/Model/Suma.java
@@ -7,7 +7,7 @@ public class Suma {
 		return n1 + n2;
 	}
 	
-	public int sumarVariosNumeros(int [] numeros)
+	public int sumarVariosNumeros(int [] numeros) // 2xy31
 	{
 		int resultado = 0;
 		for(int n: numeros)
@@ -17,5 +17,21 @@ public class Suma {
 		return resultado;
 	}
 	
+	public long sumarVariosNumeros(long [] numeros)
+	{
+		int resultado = 0;
+		for(long n: numeros)
+		{
+			resultado += n;
+		}
+		return resultado;
+	}
+	
+	public int sumarAbsolutos(int n1, int n2)
+	{
+		int numeroAbsoluto1 = Math.abs(n1);
+		int numeroAbsoluto2 = Math.abs(n2);
+		return numeroAbsoluto1 + numeroAbsoluto2;
+	}
 	
 }
